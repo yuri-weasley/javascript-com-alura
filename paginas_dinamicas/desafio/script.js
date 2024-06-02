6a625d04e6cd1125a883cf2349cf88eaae580191
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-06-02 16:14:11 -0400

    funcionalidade das teclas do alura fone

--- paginas_dinamicas/desafio/script.js
+++ paginas_dinamicas/desafio/script.js
@@ -1,1 +1,10 @@
-document.querySelector('input');
+const listaDeTeclas = document.querySelectorAll('input[type=button]');
+const campo = document.querySelector('input[type=tel]');
+
+for (var i = 0; i < listaDeTeclas.length; i++) {
+  const tecla = listaDeTeclas[i];
+  
+  tecla.onclick = function() {
+    campo.value = campo.value + tecla.value;
+  }
+}
