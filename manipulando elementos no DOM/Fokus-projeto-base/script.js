c277a7e473800f7372dc057500be8f891536d557
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-06-04 18:07:40 -0400

    modificando imagens

--- manipulando elementos no DOM/Fokus-projeto-base/script.js
+++ manipulando elementos no DOM/Fokus-projeto-base/script.js
@@ -4,7 +4,7 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 
 const temp = document.querySelector('.app__card-timer');
-const imagem = document.querySelector('.app__image-figure'); //*
+const imagem = document.querySelector('.app__image');
 const frase = document.querySelector('.app__title');
 var btnInicio = document.querySelector('.app__card-primary-button');
 var btnFoco = document.querySelector('.app__card-button--foco');
@@ -16,12 +16,15 @@ const tempoLongo = 900;
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
+    imagem.setAttribute('src', 'imagens/foco.png');
 })
 
 curtoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    imagem.setAttribute('src', 'imagens/descanso-curto.png');
 })
 
 longoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    imagem.setAttribute('src', 'imagens/descanso-longo.png');
 })
