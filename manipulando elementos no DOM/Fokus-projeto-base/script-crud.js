4c34d5ed8b591ebd843e17ca9e3ec4dd877c9bec
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-06-10 15:02:18 -0400

    update

--- manipulando elementos no DOM/Fokus-projeto-base/script-crud.js
+++ manipulando elementos no DOM/Fokus-projeto-base/script-crud.js
@@ -8,22 +8,23 @@ const ulTarefas = document.querySelector('.app__section-task-list');
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
 function criarElementoTarefa(tarefa) {
-    const li = document.criarElemento('li');
+    const li = document.createElement('li');
     li.classList.add('app__section-task-list-item');
 
     const svg = document.createElement('svg');
     svg.innerHTML = `
-    <svg>
-        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-        </svg>
     </svg>
     `
     const paragrafo = document.createElement('p');
     paragrafo.textContent = tarefa.descricao;
+    paragrafo.classList.add('app__section-task-list-item-description');
 
     const botao = document.createElement('button');
+    botao.classList.add('app_button-edit');
+
     const imagemBotao = document.createElement('img');
     imagemBotao.setAttribute('src', 'imagens/edit.png');
     botao.append(imagemBotao);
@@ -45,8 +46,12 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
         descricao: textArea.value
     }
     tarefas.push(tarefa);
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
-})
+    textArea.value = ''
+    formAdicionarTarefa.classList.add('.hidden');
+});
 
 tarefas.forEach(tarefa => {
    const elementoTarefa = criarElementoTarefa(tarefa);
