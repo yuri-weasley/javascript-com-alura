2701ba9cff0bd8d54821b8faabd1054051ce4af1
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-06-16 07:56:46 -0400

    finishing the voice recognition

--- numero_secreto/app/reconhecimentoDeVoz.js
+++ numero_secreto/app/reconhecimentoDeVoz.js
@@ -1,3 +1,5 @@
+const elementoChute = document.getElementById('chute');
+
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 
@@ -5,3 +7,17 @@ window.SpeechRecognition =
 
   recognition.lang = 'pt-Br';
   recognition.start();
+
+  recognition.addEventListener('result', onSpeak)
+
+  function onSpeak(e) {
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+  }
+
+  function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+      <div>Você disse:</div>
+      <span class="box">${chute}</span>
+    `
+  }
