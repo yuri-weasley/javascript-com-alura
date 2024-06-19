48501abcf0fdf6beb8fb6e2183db4c653d17619f
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-06-19 08:46:41 -0400

    api funcionando e página carregando os dados de lá

--- projeto-vidflow-main/projeto-vidflow-main/script.js
+++ projeto-vidflow-main/projeto-vidflow-main/script.js
@@ -1,15 +1,18 @@
-const containerVideos = document.querySelector('.videos__container');
+const containerVideos = document.querySelector(".videos__container");
 
 const api = fetch("http://localhost:3000/videos")
 .then(res => res.json())
 .then((videos) => 
-    videos.forEach((video) => {
+    videos.forEach((video)=> {
         containerVideos.innerHTML += `
         <li class="videos__item">
-            <iframe src="${video.url}" title="${videos.titulo}" frameborder="0" allowfullscreen></iframe>
-            
+            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
+                <h3 class="titulo-video">${video.titulo}</h3>
+                <p class="titulo-canal">${video.descricao}</p>
+            </div>
         </li>
         `;
-    })
-
+        })
 )
