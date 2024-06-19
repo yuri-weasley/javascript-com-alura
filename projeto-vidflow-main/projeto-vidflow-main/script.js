1231df9c2b001505d6171fee1a7e8e461106fc87
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-06-19 08:07:27 -0400

    adicionando a api

---
+++ projeto-vidflow-main/projeto-vidflow-main/script.js
@@ -0,0 +1,15 @@
+const containerVideos = document.querySelector('.videos__container');
+
+const api = fetch("http://localhost:3000/videos")
+.then(res => res.json())
+.then((videos) => 
+    videos.forEach((video) => {
+        containerVideos.innerHTML += `
+        <li class="videos__item">
+            <iframe src="${video.url}" title="${videos.titulo}" frameborder="0" allowfullscreen></iframe>
+            
+        </li>
+        `;
+    })
+
+)
