2a81c4beff61e888543b7de8cdb6b4a6839c4e5f
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-06-20 08:10:01 -0400

    tratando e prevenindo erros

--- projeto-vidflow-main/projeto-vidflow-main/script.js
+++ projeto-vidflow-main/projeto-vidflow-main/script.js
@@ -2,29 +2,36 @@ const containerVideos = document.querySelector(".videos__container");
 
 
 async function buscarEMostrarVideos() {
-    const busca = await fetch("http://localhost:3000/videos")
-    const videos = await busca.json();
+        try{
+        const busca = await fetch("http://localhost:3000/videos")
+        const videos = await busca.json();
 
-    /*.then(res => res.json()) /*Callback - Pegar as resposta do fetch e transformar em .JSON
-    .then((videos) => /*Caminha por todos os vídeos que pegamos do .JSON e aplicar no HTML*/
+        /*.then(res => res.json()) /*Callback - Pegar as resposta do fetch e transformar em .JSON
+        .then((videos) => /*Caminha por todos os vídeos que pegamos do .JSON e aplicar no HTML*/
 
-        videos.forEach((video)=> {
-            containerVideos.innerHTML += `
-            <li class="videos__item">
-                <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-                <div class="descricao-video">
-                    <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
-                    <h3 class="titulo-video">${video.titulo}</h3>
-                    <p class="titulo-canal">${video.descricao}</p>
-                </div>
-            </li>
-            `;
-            })
-    /*
-    .catch((error) => {
+            videos.forEach((video)=> {
+                if(video.categoria = "") {
+                    throw new Error('Vídeo não tem categoria');
+                }
+                containerVideos.innerHTML += `
+                <li class="videos__item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                    </div>
+                </li>
+                `;
+                })
+        /*
+        .catch((error) => {
+            containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
+        })
+        */
+    } catch(error) {
         containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
-    })
-    */
-}
+    } 
+} 
 
 buscarEMostrarVideos();
