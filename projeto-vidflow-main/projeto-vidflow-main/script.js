6fd80c6bb80770d416d7c2c5d290fbcc1441b698
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-06-21 11:06:23 -0400

    terminando o vidflow há um bug nos filtros ainda

--- projeto-vidflow-main/projeto-vidflow-main/script.js
+++ projeto-vidflow-main/projeto-vidflow-main/script.js
@@ -20,6 +20,7 @@ async function buscarEMostrarVideos() {
                         <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
                         <h3 class="titulo-video">${video.titulo}</h3>
                         <p class="titulo-canal">${video.descricao}</p>
+                        <p class="categoria" hidden>${video.categoria}</p>
                     </div>
                 </li>
                 `;
@@ -35,3 +36,48 @@ async function buscarEMostrarVideos() {
 } 
 
 buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector('.pesquisar__input');
+
+barraDePesquisa.addEventListener('input', filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll('.videos__item');
+
+    if(barraDePesquisa.value != '') {
+        for(var video of videos) {
+            var titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+            var valorFiltro = barraDePesquisa.value.toLowerCase();
+
+            if(!titulo.includes(valorFiltro)){
+                video.style.display = 'none';
+            } else {
+                video.style.display = 'block';
+            }
+
+        }
+    } else {
+        video.style.display = 'block';
+    }
+}
+
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+    var nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+})
+
+function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll(".videos__item");
+    for(var video of videos){
+        var categoria = video.querySelector(".categoria").textContent.toLowerCase();
+        var valorFiltro = filtro.toLowerCase();
+
+        if(!categoria.includes(valorFiltro) && valorFiltro != 'tudo'){
+            video.style.display = "none";
+        } else {
+            video.style.display = "block";
+        }
+    }
+}
