0618a21433cce88a03285835ded54cfa920e5eb5
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2025-09-04 21:20:09 -0400

    Deleted aluraBooks

--- alura_books-projeto_inicial/app/metodoReduce.js
+++
@@ -1,3 +0,0 @@
-function calcularValorTotalDeLivrosDisponiveis(livros) {
-    return livros.reduce((acc, livros) => acc + livros.preco, 0).toFixed(2)
-}
--- alura_books-projeto_inicial/app/metodoSort.js
+++
@@ -1,7 +0,0 @@
-var btnOrdenarPorPreco = document.getElementById('btnOrdenarPorPreco')
-btnOrdenarPorPreco.addEventListener('click', ordenarLivrosPorPreco)
-
-function ordenarLivrosPorPreco() {
-    var livrosOrdenados = livros.sort((a, b) => a.preco - b.preco)
-    exibirOsLivrosNaTela(livrosOrdenados)
-}
